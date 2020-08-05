4110647e912a9d1d67f9d0a60b2035dfa243d56e
GrahamEmory <gemory44@gmail.com>
2020-08-05 21:07:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4 
+# 1 2 3 4 5
+
+# for i in range(1, 7):
+#   for j in range(1, i):
+#     print(j, end = "")
+#   print()
+
+# for i in range(5):
+#   print(i)
+
+
+#for i in range(5):
+  #for j in range(4):
+    #print("hello")
+
+
+num = input("Enter any number: ")
+num = float(num)
+
+if (num < 0):
+  print("that is a negative number")
+elif (num == 0):
+  print("that is neither a positive nor a negative number")
+else:
+  print("that is a positive number")
+
+
+# condition to assess whether num is possitive, negative, or zero.
